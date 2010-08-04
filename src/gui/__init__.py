38fa4a718e3a9aeb77cc102e33e0315d517785e8
Philipp Meier <pmeier82@googlemail.com>
2010-08-04 19:43:02 +0200

    ui files and new resource for the gui package

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 ################################################################################
 ##
 ##  Copyright 2010 Philipp Meier <pmeier82@googlemail.com>
